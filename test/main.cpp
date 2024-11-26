61874fe2c20024616dbc9f1d67904c966adec011
Lasse <lap@di.ku.dk>
2024-11-26 20:09:59 +0100

    basic setup

--- test/main.cpp
+++ test/main.cpp
@@ -5,13 +5,57 @@
 #include <iostream>
 #include <algorithm>
 #include "stdlib.h"
+#include <thread>
+#include <atomic>
+#include <array>
 
 #include "Tracer.h"
+#include "NodeFactory.h"
+#include "Node.h"
+
+std::string receiverPropertyFiles[] = {
+    "receiver0_middle.properties",
+    "receiver1_middle.properties",
+    "receiver0_bottom.properties"
+};
+
+std::string requesterPropertyFiles[] = {
+    "requester0.properties"
+};
+
+void run(Node* node) {
+    node->start();
+}
 
 int main(int argc, char* argv[]) {
-  Tracer tracer;
+    NodeFactory nodeFactory;
 
-  tracer.initialize();  
+    if(argc != 2) {
+        std::cout << "Give a path to the config folder" << std::endl;
+        throw;
+    }
+
+    std::string basePath { argv[1] };
+
+    std::vector<Node*> receiverNodes;
+    std::vector<std::thread> threads;
+
+    for(std::size_t i = 0 ; i < std::size(receiverPropertyFiles) ; i++) {
+        Node* node = nodeFactory.createNode(basePath + receiverPropertyFiles[i]);
+        receiverNodes.push_back(node);
+        threads.emplace_back(run, node);        
+    }
+
+    while(!std::all_of(receiverNodes.begin(), receiverNodes.end(), [](Node* node) { return node->hasStarted(); })) {}
+    
+    for(std::size_t i = 0 ; i < std::size(requesterPropertyFiles) ; i++) {
+        threads.emplace_back(run, nodeFactory.createNode(basePath + requesterPropertyFiles[i]));                
+    }
+
+    // join all
+    for(auto it = threads.begin() ; it != threads.end() ; it++) {
+        it->join();
+    }
 }         
 
 
