6066487b996a9494564fe46192491d58922fe7d4
Lasse <lap@di.ku.dk>
2024-11-30 20:33:05 +0100

    lockless queue

---
+++ src/storage/network/NetworkStorage.h
@@ -0,0 +1,85 @@
+#ifndef TRACE_STORAGE_NETWORK_NETWORK_STORAGE_H
+#define TRACE_STORAGE_NETWORK_NETWORK_STORAGE_H
+
+#include <chrono>
+#include <string>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string>
+#include <vector>
+
+#include "../AbstractStorage.h"
+
+
+template <class aaaa>
+class NetworkStorage : public AbstractStorage
+{
+
+private:
+
+  int socketDescriptor;
+
+  uint16_t port;
+
+  uint32_t ipv4;
+
+  std::vector<std::string> toSend;
+
+
+public:
+  NetworkStorage() {
+    // could also be SOCK_DGRAM for udp
+    this->socketDesriptor = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+
+    if (socketDesriptor == -1) {
+      throw;
+    }
+
+    // need to set SO_KEEPALIVE on socket and handle broken connection
+  }
+
+  // only used for tcp, not udp
+  void connect() {
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(this->port),
+        .sin_addr = {
+            .s_addr = this->ipv4}};
+
+    int ret = connect(this->socketDescriptor, &addr, sizeof(addr));
+
+    if (ret == -1) {
+      throw;
+    }
+  }
+
+  // may only be called once should not be called when there's pending IO.
+  void close() {
+    int ret = close(socketDescriptor);
+
+    if (ret == -1) {
+      throw;
+    }
+  }
+
+  void write(std::string& bytes) {
+
+    if(toSend.size() > 0) {
+      
+    }
+
+
+
+
+    size_t bytesSend = send(socketDescriptor, bytes.data(), bytes.size());
+
+    if (bytesSend == -1) {
+      throw;
+    } else if(bytesSend < bytes.size()) {
+      
+    }
+  }
+
+};
+
+#endif // TRACE_STORAGE_NETWORK_NETWORK_STORAGE_H
---
+++ src/utility/ProdConsQueue.h
@@ -0,0 +1,57 @@
+#ifndef TRACE_UTILITY_PROD_CONS_QUEUE_H
+#define TRACE_UTILITY_PROD_CONS_QUEUE_H
+
+#include <chrono>
+#include <atomic>
+
+template<class ITEM_T, unsigned int SIZE>
+class ProdConsQueue {
+
+    private:
+
+        unsigned int = SIZE;
+
+        std::atomic<int> readIdx {0};
+        std::atomic<int> writeIdx {0};
+
+        ITEM_T buffer[SIZE];
+
+    public:
+
+        void push(ITEM_T&& item) {
+            int writeIndex = writeIdx.load(std::memory_order_relaxed);
+
+            if(writeIndex + 1 != readIdx.load(std::memory_order_acquire)) {
+                buffer[writeIdx] = std::move(item);
+                writeIdx.store(++writeIdx % SIZE, std::memory_order_release);
+            } else {
+                throw; // queue full
+            }
+        }
+
+        void pop() {
+            int readIndex = readIdx.load(std::memory_order_relaxed);
+
+            if(readIndex != writeIdx.load(std::memory_order_acquire)) {
+                buffer[readIdx].~ITEM_T();
+                readIdx.store(++readIdx % SIZE, std::memory_order_release);
+            } else {
+                throw; // not data in queue
+            }
+        }
+
+        ITEM_T* front() {
+            int readIndex = readIdx.load(std::memory_order_relaxed);
+
+            if(readIndex != writeIdx.load(std::memory_order_acquire)) {
+                return &buffer[readIdx];                
+            } 
+            
+            return nullptr;            
+        }
+        
+        
+};
+
+
+#endif // TRACE_UTILITY_PROD_CONS_QUEUE_H
